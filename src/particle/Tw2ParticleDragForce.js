b457405716979c786d47a6d2134c3f6bac08d88e
cppctamber <pauloosterman@gmail.com>
2016-01-24 15:42:38 +1300

    Minor fixes to Particle constructors and Basic Documentation
    - Basic Documentation
    - Fixed some duplicate declared variables

--- src/particle/Tw2ParticleDragForce.js
+++ src/particle/Tw2ParticleDragForce.js
@@ -1,14 +1,29 @@
-﻿function Tw2ParticleDragForce()
+/**
+ * Tw2ParticleDragForce
+ * @property {number} drag
+ * @constructor
+ */
+function Tw2ParticleDragForce()
 {
     this.drag = 0.1;
 }
 
-Tw2ParticleDragForce.prototype.ApplyForce = function (position, velocity, force)
+/**
+ * ApplyForce
+ * @param position
+ * @param velocity
+ * @param force
+ * @prototype
+ */
+Tw2ParticleDragForce.prototype.ApplyForce = function(position, velocity, force)
 {
     force[0] += velocity.buffer[velocity.offset] * -this.drag;
     force[1] += velocity.buffer[velocity.offset + 1] * -this.drag;
     force[2] += velocity.buffer[velocity.offset + 2] * -this.drag;
 };
 
-Tw2ParticleDragForce.prototype.Update = function () { };
-
+/**
+ * Internal render/update function. It is called every frame.
+ * @prototype
+ */
+Tw2ParticleDragForce.prototype.Update = function() {};
