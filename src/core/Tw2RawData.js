8eec190454687bcf990d39573537da69a28b04f6
cppctamber <pauloosterman@gmail.com>
2016-01-30 18:40:50 +1300

    Minor fixes and Basic Documentation Tw2RawData
    - Basic Documentation
    - Added `hasOwnProperty` check to `for in` loop

--- src/core/Tw2RawData.js
+++ src/core/Tw2RawData.js
@@ -1,3 +1,13 @@
+/**
+ * Stores raw data for {@link Tw2PerObjectData} perObject objects and {@link EveSpaceScene} perFrame objects
+ * @property {number} nextOffset
+ * @property {Float32Array} data
+ * @property {Object} elements
+ * @property {String} elements.offset
+ * @property {number} elements.size
+ * @property {Array|null} elements.array
+ * @constructor
+ */
 function Tw2RawData()
 {
     this.nextOffset = 0;
@@ -5,33 +15,70 @@ function Tw2RawData()
     this.elements = {};
 }
 
-Tw2RawData.prototype.Declare = function (name, size)
+/**
+ * Declares a raw data element
+ * @param {String} name
+ * @param {number} size
+ * @prototype
+ */
+Tw2RawData.prototype.Declare = function(name, size)
 {
-    this.elements[name] = {'offset': this.nextOffset, 'size': size, 'array': null};
+    this.elements[name] = {
+        'offset': this.nextOffset,
+        'size': size,
+        'array': null
+    };
     this.nextOffset += size;
 };
 
-Tw2RawData.prototype.Create = function ()
+/**
+ * Create
+ * @prototype
+ */
+Tw2RawData.prototype.Create = function()
 {
     this.data = new Float32Array(this.nextOffset);
     for (var el in this.elements)
     {
-        this.elements[el].array = this.data.subarray(this.elements[el].offset, this.elements[el].offset + this.elements[el].size);
+        if (this.elements.hasOwnProperty(el))
+        {
+            this.elements[el].array = this.data.subarray(this.elements[el].offset, this.elements[el].offset + this.elements[el].size);
+        }
     }
 };
 
-Tw2RawData.prototype.Set = function (name, value)
+/**
+ * Sets a element value
+ * @param {string} name
+ * @param {Float32Array} value
+ * @prototype
+ */
+Tw2RawData.prototype.Set = function(name, value)
 {
     var el = this.elements[name];
     this.data.set(value.length > el.size ? value.subarray(0, el.size) : value, el.offset);
 };
 
-Tw2RawData.prototype.Get = function (name)
+/**
+ * Gets an element's array value
+ * TODO: Modifying the returned value will modify the raw data, is this intentional?
+ * @param {string} name
+ * @return {Float32Array}
+ * @prototype
+ */
+Tw2RawData.prototype.Get = function(name)
 {
     return this.elements[name].array;
 };
 
-Tw2RawData.prototype.GetData = function (name) 
+/**
+ * Gets an element's array value from the share data array
+ * TODO: Modifying the returned value will modify the raw data, is this intentional?
+ * @param {string} name
+ * @return {Float32Array}
+ * @prototype
+ */
+Tw2RawData.prototype.GetData = function(name)
 {
     return this.data.subarray(this.elements[name].offset, this.elements[name].offset + this.elements[name].array.length);
 };
