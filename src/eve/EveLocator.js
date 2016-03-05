32c1f34affc8e710c69dff106cdc3450e0579d74
cppctamber <pauloosterman@gmail.com>
2016-03-05 20:49:25 +1300

    Basic Documentation EveLocator

--- src/eve/EveLocator.js
+++ src/eve/EveLocator.js
@@ -1,6 +1,11 @@
+/**
+ * Contains transform information for Boosters, Turrets and XLTurrets
+ * @property {string} name
+ * @property {mat4} transform
+ * @constructor
+ */
 function EveLocator()
 {
     this.name = '';
     this.transform = mat4.create();
 }
-
