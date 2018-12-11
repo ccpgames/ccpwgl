e972d123be122569a7f0085abfeb212d53ebdd6d
cppctamber <pauloosterman@gmail.com>
2018-12-11 20:52:28 +1300

    Added `Tw2PostProcess` - Not currently implemented in the post manager, but will allow post red objects to be loaded

---
+++ src/core/post/Tw2PostProcess.js
@@ -0,0 +1,18 @@
+/**
+ * Tw2PostProcess - Not implemented yet
+ *
+ * @parameter {Array<Tw2Effect>} stages
+ */
+export class Tw2PostProcess
+{
+    constructor()
+    {
+        this.stages = [];
+    }
+
+    /**
+     * Identifies that the object is not yet fully implemented
+     * @type {boolean}
+     */
+    static partialImplementation = true;
+}
